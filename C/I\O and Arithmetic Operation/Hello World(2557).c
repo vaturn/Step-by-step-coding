3324fd7a052d319cbed997a6350b9eed2c0f36b3
vaturn <72596552+vaturn@users.noreply.github.com>
2021-10-22 22:30:28 +0900

    Create Hello World(2557).c
    KR
    C언어의 'Hello World' 형태이다.
    기본적인 C언어의 출력방식은 " printf("String"); " 이다.
    
    En
    It is in the form of 'Hello World' in the C language.
    The basic output method of the C language is " printf("String"); ".

---
+++ C/I\O and Arithmetic Operation/Hello World(2557).c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!"); // 기본적인 C언어의 출력방식은 " printf("String"); " 이다.
+                            //The basic output method of the C language is " printf("String"); ".
+}
