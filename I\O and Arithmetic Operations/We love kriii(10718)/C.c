1d86e4c1851c8883e74f723f3d76a051e7e0a018
vaturn <72596552+vaturn@users.noreply.github.com>
2021-11-01 22:27:48 +0900

    Create C.c

---
+++ I\O and Arithmetic Operations/We love kriii(10718)/C.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("강한친구 대한육군\n"); // '\n' 은 출력할 때 한줄을 띄어 쓴다는 의미이다.
+    printf("강한친구 대한육군");   // '\n' means writing a separate line when outputting.
+    
+    return 0;
+}
