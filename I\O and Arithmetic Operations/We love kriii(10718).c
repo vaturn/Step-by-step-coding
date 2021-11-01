b13dc266bcab040d04038b65411290b11cf8da6d
vaturn <72596552+vaturn@users.noreply.github.com>
2021-11-01 22:24:40 +0900

    Delete We love kriii(10718).c

--- I\O and Arithmetic Operations/We love kriii(10718).c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("강한친구 대한육군\n"); // '\n' 은 출력할 때 한줄을 띄어 쓴다는 의미이다.
-    printf("강한친구 대한육군");   // '\n' means writing a separate line when outputting.
-    
-    return 0;
-}
